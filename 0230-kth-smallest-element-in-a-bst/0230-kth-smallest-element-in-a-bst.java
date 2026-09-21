0ec1f6ed27d0f718155726a3be318a9ccc5182a6
Ayushhx24 <ayushh.x24@gmail.com>
2026-09-21 19:46:24 +0530

    Time: 0 ms (100%), Space: 46.7 MB (51.6%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,20 +15,21 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        Stack<TreeNode> stack = new Stack<>();
         TreeNode curr = root;
-        
-        while (curr != null || !stack.isEmpty()) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        int kth = 0;
+        while (!stack.isEmpty() || curr != null) {
             while (curr != null) {
                 stack.push(curr);
                 curr = curr.left;
             }
             curr = stack.pop();
-            k--;
-            if (k == 0) {
-                return curr.val;
-            }
+            kth++;
+            if (kth == k) return curr.val;
+
             curr = curr.right;
         }
-        return -1;
-}}
+
+        return curr.val;
+    }
+}
